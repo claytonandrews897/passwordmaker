4752e0f18781d7ee1e3d933b352e5fa975777309
Clayton Andrews <claytonandrews897@gmail.com>
2024-02-08 14:46:01 -0500

    test

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,22 @@
 // Assignment code here
 
-
+let listUppercaseLetters = ["A","B","C"]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+//console.log(generateBtn)
+function generatePassword(){
+ let lenghtOfPassword = prompt("what is the length of you password?");
+ console.log(lenghtOfPassword)
+}
 // Write password to the #password input
 function writePassword() {
+
+
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
+
+   var passwordText = document.querySelector("#password");
+   
 
   passwordText.value = password;
 
