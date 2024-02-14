5bd6cf39f42debef3154b97bb5a77ab6a571f784
Clayton Andrews <claytonandrews897@gmail.com>
2024-02-14 17:35:24 -0500

    uodated the script.js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,32 @@
 // Assignment code here
 
-let listUppercaseLetters = ["A","B","C"]
+let listUppercaseLetters = ["A","B","C","D","E","F","G"]
+let listLowercaseLetters = ["a","b","c","d","e","f","g"]
+let listNumbers = ["7","6","5","4","3","2","1"]
+let listSpecial = ["!","@","#","%","&","?"]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 //console.log(generateBtn)
 function generatePassword(){
  let lenghtOfPassword = prompt("what is the length of you password?");
+ let confirmUppercaseLetters = confirm("Would you like Uppercase letters?")
+ let confirmLowercaseLetters = confirm("Would you like lowercase letters?")
+ let confirmNumbers = confirm("Would you like numbers?")
+ let confirmSpecial = confirm("Would you like special characters?")
+ console.log(confirmUppercaseLetters)
+ var finalpassword;
+ if (confirmUppercaseLetters === true){
+    finalpassword = listUppercaseLetters[Math.floor(Math.random() * listUppercaseLetters.length)]
+  }
+
+  else {
+    console.log("i did not choose uppercase" )
+  }
+ 
+ console.log("this is final password ",finalpassword)
+ 
+ 
+ 
  console.log(lenghtOfPassword)
 }
 // Write password to the #password input
